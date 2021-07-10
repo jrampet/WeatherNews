1c86b0c1cda710dff86a874bcb8c1933ab5a12cf
Jeyaram <jram.pet@gmail.com>
2021-07-10 14:43:42 +0530

    added ScreenShots

--- WandN/WeatherCast/Current/LocationController.swift
+++ WandN/WeatherCast/Current/LocationController.swift
@@ -32,7 +32,7 @@ class LocationController: UIViewController {
     }
     func createSearch(){
         table.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-        searchBar.delegate = self
+        
         table.delegate = self
         table.dataSource = self
     }
@@ -69,3 +69,40 @@ extension LocationController:UITableViewDelegate,UITableViewDataSource,UISearchB
     
 }
 
+/*
+     var table : LocationTable?
+     var tableData = [String]()
+
+     var tableIndex = [CLLocation]()
+     var pushLocation :((CLLocation,String)->())?
+     @IBOutlet var searchBar : UITextField!
+     @IBOutlet var dataView : UIView!
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         createTable()
+
+         // Do any additional setup after loading the view.
+     }
+     func createTable(){
+         table = Bundle.main.loadNibNamed("LocationTable", owner: nil, options: nil)![0] as? LocationTable
+         table?.frame = self.dataView.bounds
+         guard let table = table else{return }
+         table.dataArray = tableData
+         table.indexArray = tableIndex
+         print(tableIndex.count)
+         self.dataView.addSubview(table)
+
+         table.getlocation = { (index) in
+
+             self.pushLocation?(self.tableIndex[index],self.tableData[index])
+             if let navController = self.navigationController {
+                 navController.popViewController(animated: true)
+             }
+         }
+
+
+     }
+
+
+ */
